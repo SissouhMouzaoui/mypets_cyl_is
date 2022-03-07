3faae59dbb3a4b3a869f9b357bfb9f8a39ca533d
SissouhMouzaoui <sissouhmouzaoui@gmail.com>
2022-03-07 19:09:51 +0100

    création du projet laravel

--- mypets/routes/web.php
+++ mypets/routes/web.php
@@ -16,13 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::resource('animals',\App\Http\Controllers\AnimalsController::class);
