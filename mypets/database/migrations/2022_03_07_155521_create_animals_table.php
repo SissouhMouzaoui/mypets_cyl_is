fa944ff4a184553e27c9e1ae50c4f493ae8c49a9
SissouhMouzaoui <sissouhmouzaoui@gmail.com>
2022-03-07 16:59:26 +0100

    modification de migration Animals et execution des migrations

---
+++ mypets/database/migrations/2022_03_07_155521_create_animals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nom');
+            $table->integer('age');
+            $table->string('descr');
+            $table->string('image');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
